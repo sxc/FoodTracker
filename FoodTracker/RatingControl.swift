7d9536206c5b9cf10a738fddef9441796836a147
Shen Xiaochun <sxc@macocoa.local>
2015-07-09 08:56:59 +0800

    add image

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Shen Xiaochun on 15/7/9.
+//  Copyright © 2015年 Sigutian,Inc. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
